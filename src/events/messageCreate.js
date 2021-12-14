c10ac868e538d350834ff676a38b5c5aa2c4805b
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-15 00:56:00 +0800

    Delete src directory

--- src/events/messageCreate.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'messageCreate',
-	once: false,
-
-	execute: () => {
-		/* */
-	},
-};
--- src/events/messageDelete.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'messageDelete',
-	once: false,
-
-	execute: () => {
-		/* */
-	},
-};
